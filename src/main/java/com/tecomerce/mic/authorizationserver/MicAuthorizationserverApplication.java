129310c5c8669817bfe942df92e1fe45aa75626d
Jesus Finol <jfinol02@gmail.com>
2024-05-04 20:58:27 +0200

    Initial commit

---
+++ src/main/java/com/tecomerce/mic/authorizationserver/MicAuthorizationserverApplication.java
@@ -0,0 +1,13 @@
+package com.tecomerce.mic.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicAuthorizationserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicAuthorizationserverApplication.class, args);
+	}
+
+}
