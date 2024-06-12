83a2102b6d4914d8c2383c9e44d58f29fc11ad78
Jesus Finol <jefinol@hiberus.com>
2024-06-12 13:16:42 +0200

    Partial commit of refactor

--- target/generated-sources/annotations/com/tecomerce/mic/authorizationserver/api/mapper/AuthorizationGrantTypeDTOMapperImpl.java
+++ target/generated-sources/annotations/com/tecomerce/mic/authorizationserver/api/mapper/AuthorizationGrantTypeDTOMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-06-12T12:51:37+0200",
+    date = "2024-06-12T13:15:48+0200",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.11 (Eclipse Adoptium)"
 )
 @Component
